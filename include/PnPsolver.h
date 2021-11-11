b553044d0e60e5d33c2d6103893f6d0c7f82ab63
Wentao Sun <sunwt5@lenovo.com>
2021-11-11 14:21:14 +0800

    Fix issues running on Windows 10 + VS2017 + OpenCV4.5.4

--- include/PnPsolver.h
+++ include/PnPsolver.h
@@ -51,7 +51,8 @@
 #ifndef PNPSOLVER_H
 #define PNPSOLVER_H
 
-#include <opencv2/core/core.hpp>
+#include <opencv2/core/core_c.h>
+//#include <opencv2/core/core.hpp>
 #include "MapPoint.h"
 #include "Frame.h"
 
